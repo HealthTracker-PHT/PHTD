522a78451cfd29fe4a8447ee1cf3c5ca45b1e279
anantachai28 <mosmoss1678@gmail.com>
2026-01-17 15:31:56 +0700

    PHTD V01.2
    -จัดเรียง code ให้ดูง่ายขึ้น

---
+++ config.js
@@ -0,0 +1,15 @@
+// Firebase Configuration
+window.firebaseConfig = {
+    apiKey: "AIzaSyDzTkR7xbg9B6UkknvRXiuamWsBR1zIQ90",
+    authDomain: "hrspo-98b0c.firebaseapp.com",
+    projectId: "hrspo-98b0c",
+    storageBucket: "hrspo-98b0c.appspot.com",
+    messagingSenderId: "364239224326",
+    appId: "1:364239224326:web:1e66faddd8e6dd2e718d87"
+};
+
+// Global Constants 
+window.APP_SETTINGS = {
+    pageSize: 10,
+    maxImageSize: 800000 // 800KB
+};
